2c49b559f36e7ff65ec485da174f0465b486dac2
Matheus Faé <matheusfae@gmail.com>
2026-09-10 18:18:13 -0300

    criado componente para menu lateral

---
+++ frontend/src/app/components/side-menu/side-menu.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-side-menu',
+  imports: [RouterLink],
+  templateUrl: './side-menu.html',
+  styleUrl: './side-menu.css',
+})
+export class SideMenu {}
