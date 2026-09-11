447c56fda908fad367eb4ed0a1446db24e9f8d45
Matheus Faé <matheusfae@gmail.com>
2026-09-11 13:54:51 -0300

    Adicionado menu lateral e routelink

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,27 +1,21 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { DevUpdateService } from './services/devupdate.services';
+import { Component, OnInit, signal } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { DevUpdate } from './models/devupdate.model';
+import { SideMenu } from './components/side-menu/side-menu';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    SideMenu,
+    RouterLink
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
 
   protected readonly title = signal('devtools-web');
   protected update?: DevUpdate;
-
-  constructor(private devUpdateService: DevUpdateService) {
-    this.devUpdateService.obter(11).subscribe({
-      next: (update) => {
-        this.update = update;
-      },
-      error: (error) => {
-        console.error('Erro ao consultar API:', error);
-      }
-    });
   }
-}
