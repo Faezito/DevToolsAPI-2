886e23461aa1f8e4b0852ac2b02f589bdef85072
Matheus Faé <matheusfae@gmail.com>
2026-09-11 11:23:37 -0300

    feat/criado componente de tabela genérica dinamica

---
+++ frontend/src/app/components/tabela-generica/tabela-generica.ts
@@ -0,0 +1,41 @@
+import { DatePipe } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+export interface Coluna<T> {
+  campo: keyof T;
+  titulo: string;
+  classe?: string;
+  formato?: string;
+}
+
+@Component({
+  selector: 'app-tabela-generica',
+  imports: [DatePipe],
+  providers: [DatePipe],
+  templateUrl: './tabela-generica.html',
+  styleUrl: './tabela-generica.css',
+})
+
+export class Tabela<T> {
+
+  constructor(private dtp: DatePipe){}
+
+  @Input()
+  dados: T[] = [];
+
+  @Input()
+  colunas: Coluna<T>[] = [];
+
+  @Output()
+  detalhe = new EventEmitter<T>();
+
+  formatarValor(item: T, coluna: Coluna<T>): unknown {
+    const valor = item[coluna.campo];
+
+    if (coluna.formato === 'data') {
+      return this.dtp.transform(valor as string | number | Date, 'dd/MM/yyyy');
+    }
+
+    return valor;
+  }
+}
